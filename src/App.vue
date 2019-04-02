ae820cb99ac18cbabe891535064a1a04cc727150
elsolenllamas <elsolenllamas@gmail.com>
2019-04-02 15:49:18 -0300

    update methods and router link

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app" class="container">
-      <input type="text"
+      <input type="text" 
+        class="form-control col-md-6" 
         @keyup.prevent="search"
         v-model="query" />
     
-    <div  class="search-results">
-        <div v-if="data" v-for="(value, index) in data" 
-          :key="index"
-          :ref="`card_${index}`"
-          class="card">
-
-            <h4>{{value._source.yourjsonfield_1}}</h4>
-            <span>{{value._source.yourjsonfield_2}}</span>
-        </div>
+    <div class="search-results-container">
+      <div v-if="data && show" v-for="(value, index) in data" 
+      :key="index"
+      :ref="`card_${index}`"
+         class="card">
+      
+      <div class="card-footer">
+        <router-link :to="{ name: 'Albums', params: { id: value._source.id , content: value._source}}">
+          <h3 v-on:click="remove">{{value._source.album_name}}</h3>
+        </router-link>
+        <h5>{{value._source.artist_name}}</h5>
+        <span>{{value._source.label}}</span>
+      </div>
     </div>
+  </div>
+  <router-view></router-view>
   </div>
 </template>
 
@@ -24,15 +31,22 @@ export default {
   data() {
     return {
       query: "",
-      data: []
+      data: [],
+      show: true
     }
   },
   methods: {
     search() {
+        this.show = true,
         this.axios.get('http://localhost:5000/search?q='+this.query)
               .then(response => {
                 this.data = response.data;
+                console.log(this.data);
           })
+    },
+
+    remove() {
+        this.show = false;
     }
   }
 }

b5f3e204f0e96bf677f7940fb3ed2ae11745dab0
elsolenllamas <elsolenllamas@gmail.com>
2019-04-02 15:45:54 -0300

    add subcompoent to show data in detail

---
+++ src/components/Albums.vue
@@ -0,0 +1,35 @@
+<template>
+
+ <div>
+    <p>Artist Name: {{ content.artist_name }}</p>
+    <p>Album Name: {{ content.album_name }}</p>
+    <p>Label: {{ content.label }}</p>
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'Albums',
+     	data () {
+            return {
+                content: {}
+            }
+        },
+
+        watch: {
+	      '$route': 'update'
+	    },
+
+        created() {
+            this.update();
+        },
+
+        methods: {
+          update() {
+              this.content = this.$route.params.content;
+          }
+		    }
+ }
+</script>

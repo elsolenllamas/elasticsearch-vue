6cb9cc50cf018ebfad06d4faf2f825513c9fb524
elsolenllamas <elsolenllamas@gmail.com>
2019-04-02 12:12:20 -0300

    remove store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
-import store from "./store";
 import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
@@ -9,6 +8,5 @@ Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount("#app");

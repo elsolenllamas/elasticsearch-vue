13f78c3fc9d537fa3a3f4b20e0a34850315f1b7b
elsolenllamas <elsolenllamas@gmail.com>
2019-04-02 11:53:26 -0300

    addData method

--- server/indexdata.js
+++ server/indexdata.js
@@ -14,3 +14,39 @@ client.indices.create({
         console.log("Successfully!", response);
     }
 });
+
+const addData = function addData(index, type, data) {
+    let dataBody = [];
+  
+    data.forEach(item => {
+      dataBody.push({
+        index: {
+          _index: index,
+          _type: type
+        }
+      });
+      dataBody.push(item);
+    });
+  
+client.bulk({body: dataBody})
+    .then(response => {
+      let errorCount = 0;
+      response.items.forEach(item => {
+        if (item.index && item.index.error) {
+          console.log(++errorCount, item.index.error);
+        }
+      });
+      console.log(
+        `Index OK`
+      );
+    })
+    .catch(console.err);
+  };
+
+async function indexDataToES() {
+   const dataSource = await fs.readFileSync('./data.json');
+   const parsedData = JSON.parse(dataSource);
+   addData('your_index', 'your_index_list', parsedData);
+}
+
+indexDataToES();
